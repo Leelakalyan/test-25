b74678f7385593ebff925954135a2337104bc419
Leelakalyan <99253536+Leelakalyan@users.noreply.github.com>
2022-02-21 21:02:43 +0530

    Create Print stars.py

---
+++ Print stars.py
@@ -0,0 +1,19 @@
+
+n=int(input('enter n value:'))
+for i in range(1,n+1):
+    for j in range(i,n+1):
+        print('*',end=' ')
+    print('\n')
+    
+Output:
+enter n value:
+5
+* * * * * 
+
+* * * * 
+
+* * * 
+
+* * 
+
+*
